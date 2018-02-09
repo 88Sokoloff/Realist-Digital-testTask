e252d5e9d0687db161dd28dea6479a2b729ed51b
88Sokoloff <88sokoloff@gmail.com>
2018-02-09 17:42:35 +0200

    complete...but fucking parallax

--- js/script.js
+++ js/script.js
@@ -9,23 +9,45 @@ var mySwiper = new Swiper('.swiper-container', {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
+	breakpoints: {
+		480: {
+			slidesPerView: 1,
+			spaceBetween: 80
+		}
+	}
 });
 
 // Parallax effect
 
-document.getElementById('move').onmousemove = function(e) {
-    var w = window.innerWidth;
-    var offsetX = 0.5 - e.pageX / w;
-    var leaf = document.getElementsByClassName('oak-leaf')[0];
-    var leaves = document.getElementsByClassName('oak-leaves')[0];
-    var offsetForM = parseInt(leaf.getAttribute('data-offset'));
-    var offsetForB = parseInt(leaves.getAttribute('data-offset'));
-    var translateM = "translate(" + Math.round(offsetX * offsetForM) + "px";
-    var translateB = "translate(-" + Math.round(offsetX * offsetForB) + "px";
-    leaf.style.webkitTransform = translateM;
-    leaf.style.MozTransform = translateM;
-    leaf.style.transform = translateM;
-    leaves.style.webkitTransform = translateB;
-    leaves.style.MozTransform = translateB;
-    leaves.style.transform = translateB;
-};
+var wrapper = document.querySelector('.move'),
+		leaf = document.querySelector('.oak-leaf'),
+		leaves = document.querySelector('.oak-leaves'),
+		r_branch = document.querySelector('.right-branch'),
+		l_branch = document.querySelector('.left-branch');
+
+wrapper.addEventListener('mousemove',function(e){
+	var pageX = e.clientX,
+			pageY = e.clientY;
+	leaf.style.transform = 'translateX(' + pageX/25 + '%) translateY(' + pageY/25 + '%)';
+	leaves.style.transform = 'translateX(' + pageX/50 + '%) translateY(' + pageY/50 + '%)';
+	// r_branch.style.width = (37 + pageX/100) + '%';
+	// l_branch.style.width = (37 + pageX/100) + '%';
+});
+
+// Accordion
+
+var acc = document.getElementsByClassName("accordion")[0],
+		conteiner = document.getElementsByClassName("header-conteiner")[0],
+		logo = document.getElementsByClassName("header-logo")[0];
+
+acc.addEventListener("click", function() {
+  this.classList.toggle("active");
+  conteiner.classList.toggle("unclipped");
+  logo.classList.toggle("moved");
+  var panel = this.nextElementSibling;
+  if (panel.style.maxHeight){
+    panel.style.maxHeight = null;
+  } else {
+    panel.style.maxHeight = panel.scrollHeight + "px";
+  } 
+});
